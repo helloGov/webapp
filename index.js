ed61b38a0ff6eac11cb68166a31704578ea7480a
Aaron Barrett <aaron@aaronbarrett.com>
2016-12-10 15:19:27 -0800

    Added `legislators-locate.js` to demonstrate how to look up the representative for a given latitude and longitude using Sunlight.

--- index.js
+++ index.js
@@ -1,23 +1,42 @@
-const path = require('path')  
-const express = require('express')  
+const path = require('path')
+const express = require('express')
 const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 8080
 
-app.engine('.hbs', exphbs({  
+app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
   layoutsDir: path.join(__dirname, 'views/layouts')
 }))
-app.set('view engine', '.hbs')  
-app.set('views', path.join(__dirname, 'views'))  
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
 
 
-app.get('/', (request, response) => {  
+app.get('/', (request, response) => {
   response.render('home', {
     name: 'Wilson'
   })
 })
 
-app.listen(port)  
+require("./legislators-locate.js");
+
+///
+// var api = require("sunlight-congress-api");
+//
+// var success = function(data){
+// 	console.log(data);
+// }
+//
+// api.init("");
+//
+// var latitude = 37.766482999;
+// var longitude = -122.417305;
+//
+// api.legislatorsLocate().filter("latitude", latitude).filter("longitude", longitude).call(success);
+
+// api.votes().filter("year", "2012").call(success);
+///
+
+app.listen(port)
---
+++ legislators-locate.js
@@ -0,0 +1,21 @@
+
+var api = require("sunlight-congress-api");
+
+var success = function(data) {
+  sunlightOutput = data;
+
+  var representative = data.results[0];
+  var title = representative.title;
+  var firstName = representative.first_name;
+  var lastName = representative.last_name;
+  var telephoneNumber = representative.phone;
+
+  console.log(title + ". " + firstName + " " + lastName + ": " + telephoneNumber);
+}
+
+api.init("");
+
+var latitude = 37.766482999;
+var longitude = -122.417305;
+
+api.legislatorsLocate().filter("latitude", latitude).filter("longitude", longitude).call(success);
