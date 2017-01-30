626a60945663c207007985028ff622cebfe164ac
sagnik <sagnik@thumbtack.com>
2017-01-30 09:46:45 -0800

    Move signup from routing to middleware

--- app/middleware/influencer.js
+++ app/middleware/influencer.js
@@ -5,20 +5,18 @@
 
 var mongoose = require("mongoose"),
  	Influencer = mongoose.model('Influencer');
-
+    passport = require('passport');
 
 var influencerController = {};
 
-
-influencerController.findInfluencer =  function (userJson){
+influencerController.findInfluencer = function (userJson) {
 
  	influencer = Influencer.findOne({"fake_id":1});
  	console.log(influencer);
  	return influencer;
- }
+}
 
- 
- influencerController.saveInfluencer = function (userJson){
+influencerController.saveInfluencer = function (userJson) {
  	userJson = {username: "Josh Canaao",
 				email: "jc@hellogov.org",
 				bio: "asd",
@@ -29,7 +27,22 @@ influencerController.findInfluencer =  function (userJson){
  	influencer = new Influencer(userJson);
  	influencer.save(function(error){console.log(error);});
  	return true;
- }
+}
 
+influencerController.addInfluencer = function (request, response) {
+    console.log(`got user_details: ${JSON.stringify(request.body)}`);
+    Influencer.register(new Influencer({ username : request.body.username }), request.body.password, function(err, account) {
+        if (err) {
+            return response.render('signup');
+            }
+
+        passport.authenticate('local')(request, response, function () {
+            response.redirect('/');
+            }, function() {
+                response.redirect('/user/login');
+                });
+        }
+    );
+}
 
 module.exports = influencerController;
--- app/routes/user.js
+++ app/routes/user.js
@@ -4,37 +4,23 @@ var Influencer = require("../models/influencer.js")
 var passport = require('passport');
 
 module.exports = function (router) {
-    
+
     router.post('/user/login', (request, response) => {
     	console.log(`got user_details: ${JSON.stringify(request.body)}`);
     	passport.authenticate('local')(request, response, function () {
 	        response.redirect('/');
 	    });
-    });  
+    });
 
     router.get('/user/login', (request, response) => {
     	response.render('login');
-    });    
-
-    router.post('/user/signup', (request, response) => {
-    	console.log(`got user_details: ${JSON.stringify(request.body)}`);
-    	Influencer.register(new Influencer({ username : request.body.username }), request.body.password, function(err, account) {
-	        if (err) {
-	            return response.render('signup');
-	        }
-
-	        passport.authenticate('local')(request, response, function () {
-	          response.redirect('/');
-	        }, function() {
-	        	response.redirect('/user/login');
-	        });
-	    });
     });
+
+    router.post('/user/signup', influencerController.addInfluencer);
 
     router.get('/user/signup', (request, response) => {
     	response.render('signup');
     });
-
 };
 
 
