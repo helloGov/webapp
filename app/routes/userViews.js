54e5a5bc119156ffb4104ce85a9d637283031486
wcrest <wcrestrepo@gmail.com>
2017-01-31 17:49:00 -0800

    fix to allow authentication on local mongo

--- app/routes/userViews.js
+++ app/routes/userViews.js
@@ -2,6 +2,9 @@
 module.exports = function (router) {
 
     router.get('/createCampaign', (request, response) => {
+		console.log(JSON.stringify(request.session));
+		console.log('Cookies: ', request.cookies);
+		console.log('Signed Cookies: ', request.signedCookies);
         response.render('create');
     });
 
