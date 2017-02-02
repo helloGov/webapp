d2b8ac5d89ab407b940f0e1cb8308bf6d79ca13d
sagnik <sagnik@thumbtack.com>
2017-02-01 18:11:21 -0800

    Add FB authentication

--- app/routes/user.js
+++ app/routes/user.js
@@ -1,7 +1,39 @@
 
 var influencerController = require("../middleware/influencer");
 var Influencer = require("../models/influencer.js")
-var passport = require('passport');
+var passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+
+var FACEBOOK_APP_ID = '1255039427879300';
+var FACEBOOK_APP_SECRET = 'fbc227cbaa110a791aaf235538470f8e';
+
+passport.use(new FacebookStrategy({
+            clientID: FACEBOOK_APP_ID,
+            clientSecret: FACEBOOK_APP_SECRET,
+            callbackURL: "http://localhost:8080/auth/facebook/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            return cb(null, profile);
+        }
+    )
+);
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  In a
+// production-quality application, this would typically be as simple as
+// supplying the user ID when serializing, and querying the user record by ID
+// from the database when deserializing.  However, due to the fact that this
+// example does not have a database, the complete Facebook profile is serialized
+// and deserialized.
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
 
 module.exports = function (router) {
 
@@ -16,16 +48,27 @@ module.exports = function (router) {
     	response.render('login');
     });
 
-    router.post('/user/fb_login', (request, response) => {
-        console.log(`got user_details: ${JSON.stringify(request.body)}`);
-        passport.authenticate('local')(request, response, function () {
-            response.redirect('/');
-        });
-    });
+    router.get('/user',
+      function(req, res) {
+        console.log(req.user);
+        res.render('user_test', { user: req.user });
+      });
 
-    router.get('/user/fb_login', (request, response) => {
-        response.render('login');
-    });
+    // Redirect the user to Facebook for authentication.  When complete,
+    // Facebook will redirect the user back to the application at
+    //     /auth/facebook/callback
+    router.get('/auth/facebook', passport.authenticate('facebook'));
+
+    // Facebook will redirect the user to this URL after approval.  Finish the
+    // authentication process by attempting to obtain an access token.  If
+    // access was granted, the user will be logged in.  Otherwise,
+    // authentication has failed.
+    router.get('/auth/facebook/callback',
+        passport.authenticate('facebook', { failureRedirect: '/user/login' }),
+        function(req, res) {
+          console.log(`got user_details: ${JSON.stringify(req.body)}`);
+          res.redirect('/user');
+        });
 
     router.post('/user/signup', influencerController.addInfluencer);
 
