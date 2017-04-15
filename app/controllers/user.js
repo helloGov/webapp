abd99a9263f521d973c616095ef574db2ca62f5d
Tim Gupta <tjgupta@gmail.com>
2017-04-15 02:11:02 -0700

    Set up profile settings editing.

---
+++ app/controllers/user.js
@@ -0,0 +1,21 @@
+var Signup = require('../models/signup');
+var passport = require('passport');
+
+var userController = {};
+
+userController.addUserWithFacebook = function(request, response) {
+    var findStr = { email: request.body.email, signupLink: request.body.signupLink };
+    Signup.findOne(findStr).exec()
+    .then(function(signup) {
+        if (signup) {
+            passport.authenticate('facebook');
+        } else {
+            console.log('User not found in signup database! User: ' + request.body.email + ' link: ' + request.body.signupLink);
+        }
+    })
+    .catch(function(reason) {
+        console.log('Error! Could not find signup link: ' + reason);
+    });
+};
+
+module.exports = userController;
--- app/routes/campaign.js
+++ app/routes/campaign.js
@@ -3,9 +3,8 @@ const Campaign = require('../models/campaign');
 module.exports = function(router) {
     // create campaign page
     router.get('/createCampaign', (request, response) => {
-        console.log(`${JSON.stringify(request.user)}`);
         if (request.user) {
-            response.render('create', {logged_in: true});
+            response.render('create', {user: request.user, logged_in: true});
         } else {
             response.redirect('/login');
         }
@@ -16,9 +15,8 @@ module.exports = function(router) {
         if (request.user) {
             Campaign.findById(request.params.shortid)
                 .then(function(campaign) {
-                    console.log(campaign);
                     if (request.user.id === campaign.influencer) {
-                        response.render('create', {campData: campaign, logged_in: true});
+                        response.render('create', {user: request.user, campData: campaign, logged_in: true});
                     } else {
                         response.status(301).render('unauthorized', {logged_in: true});
                     }
@@ -52,12 +50,12 @@ module.exports = function(router) {
 
     // campaign list page
     router.get('/campaigns', (request, response) => {
-        response.render('campaigns', {logged_in: request.user != null});
+        response.render('campaigns', {user: request.user, logged_in: request.user != null});
     });
 
     // campaign success page (at the end of campaign creation)
     router.get('/campaignSuccess', (request, response) => {
-        response.render('campaignSuccess', {logged_in: request.user != null});
+        response.render('campaignSuccess', {user: request.user, logged_in: request.user != null});
     });
 
     // campaign call page (available to all visitors)
@@ -65,10 +63,10 @@ module.exports = function(router) {
         Campaign.findById(request.params.shortid)
             .then(function(campaign) {
                 if (campaign.publish) {
-                    response.render('campaign', {campData: campaign, logged_in: request.user != null});
+                    response.render('campaign', {user: request.user, campData: campaign, logged_in: request.user != null});
                 } else {
                     console.log(`Couldn't load campaign page for ${request.params.shortid}`);
-                    response.status(404).render('404', {logged_in: request.user != null});
+                    response.status(404).render('404', {user: request.user, logged_in: request.user != null});
                 }
             });
     });
@@ -78,10 +76,10 @@ module.exports = function(router) {
         Campaign.findById(request.params.shortid)
             .then(function(campaign) {
                 if (campaign) {
-                    response.render('thankYou', {campData: campaign, logged_in: request.user != null});
+                    response.render('thankYou', {user: request.user, campData: campaign, logged_in: request.user != null});
                 } else {
                     console.log(`Couldn't find thank you page for ${request.params.shortid}`);
-                    response.status(404).render('404', {logged_in: request.user != null});
+                    response.status(404).render('404', {user: request.user, logged_in: request.user != null});
                 }
             });
     });
