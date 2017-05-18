4da0f3d5126e811c528a7a16135f8ba521bcce91
Tim Gupta <tjgupta@gmail.com>
2017-05-17 22:41:13 -0700

    Move forgot password stuff to client templates. UX tweaks.

---
+++ public/js/account/forgotPassword.component.js
@@ -0,0 +1,30 @@
+import angular from 'angular';
+
+export default angular.module('helloGov')
+.component('forgotPasswordForm', {
+    template: require('./forgotPassword.html'),
+    controller: function($http, $window, constants) {
+        'ngInject';
+        const self = this;
+        this.emailRequestDetails = {};
+
+        this.submitDisabled = false;
+
+        this.requestReset = function() {
+            self.submitDisabled = true;
+            self.message = '';
+            self.error = '';
+            $http.post(`${constants.API_ROOT}/auth/password/requestReset`, self.emailRequestDetails)
+                .then(function(data) {
+                    self.message = 'An email has been sent to you. Please check your inbox for instructions to reset your password. If you do not receive this email, please check your spam folder.';
+                })
+                .catch(function() {
+                    self.error = 'There was an error. Please try again later.';
+                })
+                .finally(function() {
+                    self.submitDisabled = false;
+                });
+        };
+    }
+})
+.name;
