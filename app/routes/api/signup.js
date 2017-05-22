68b5d2f74215acc5fc44fb7a4e9e8f0de9258073
Tim Gupta <tjgupta@gmail.com>
2017-05-21 22:53:53 -0700

    Beef up admin signup stuff.

---
+++ app/routes/api/signup.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const router = express.Router();
+const Signup = require('../../models/signup');
+const User = require('../../models/user');
+
+// Signup list
+router.route('/signups')
+
+.post((request, response) => {
+    if (request.user && request.user.admin) {
+        let email = request.body.email;
+
+        // don't add if Signup already exists
+        Signup.findOne({email: email})
+            .then(function(signup) {
+                if (signup) {
+                    throw new Error('Signup already exists with this email address.');
+                }
+            })
+            // check to see if user has already created an account with this email
+            .then(function() {
+                return User.findOne({email: email})
+                    .then(function(user) {
+                        if (user) {
+                            throw new Error('User account already exists with this email address.');
+                        }
+                    });
+            })
+            .then(function() {
+                var signup = new Signup({email: email});
+                return signup.save();
+            })
+            .then(function(signup) {
+                return response.send(signup);
+            })
+            .catch(function(err) {
+                response.status(400).send({
+                    error: {
+                        statusCode: 400,
+                        status: 'Bad request',
+                        message: 'There was an error creating new signup: ' + err
+                    }
+                });
+            });
+    } else {
+        response.status(403).end();
+    }
+})
+
+.get((request, response) => {
+    if (request.user && request.user.admin) {
+        Signup.find().sort('-createdAt')
+            .then((signups) => {
+                return response.send({result: signups});
+            });
+    } else {
+        return response.status(403).end();
+    }
+});
+
+module.exports = router;
