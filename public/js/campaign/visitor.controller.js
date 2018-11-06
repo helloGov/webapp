237241655710200e8a8995fda6ab26ab33624012
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-06 14:15:04 -0800

    reformat bracket spacing

--- public/js/campaign/visitor.controller.js
+++ public/js/campaign/visitor.controller.js
@@ -1,45 +1,43 @@
 import angular from 'angular';
 
 export default angular.module('helloGov')
-.controller('visitorController', function($scope, $http, $window, $location, constants) {
-    'ngInject';
-    // FIXME: this is super brittle to get the campaignId like this, but we're not using angular's
-    // routing so it's not possible to get it using angular yet
-    var urlSplit = $location.absUrl().split('/');
-    $scope.campaign = urlSplit.pop();
-    $scope.locResult = '';
-    $scope.locOptions = null;
-    $scope.locDetails = '';
+    .controller('visitorController', function ($scope, $http, $window, $location, constants) {
+        'ngInject';
+        // FIXME: this is super brittle to get the campaignId like this, but we're not using angular's
+        // routing so it's not possible to get it using angular yet
+        var urlSplit = $location.absUrl().split('/');
+        $scope.campaign = urlSplit.pop();
+        $scope.locResult = '';
+        $scope.locOptions = null;
+        $scope.locDetails = '';
 
-    $scope.sendEvent = function(type) {
-        $http.post(`${constants.API_ROOT}/events`, {type: type, campaign: $scope.campaign});
-    };
-    $scope.sendEvent('visit');
-    $scope.update = function() {
-        var coordinates = {
-            'latitude': $scope.locDetails.geometry.location.lat(),
-            'longitude': $scope.locDetails.geometry.location.lng()
+        $scope.sendEvent = function (type) {
+            $http.post(`${constants.API_ROOT}/events`, { type: type, campaign: $scope.campaign });
+        };
+        $scope.sendEvent('visit');
+        $scope.update = function () {
+            var coordinates = {
+                'latitude': $scope.locDetails.geometry.location.lat(),
+                'longitude': $scope.locDetails.geometry.location.lng()
+            };
+            $http.get('/locateLegislator', { params: coordinates })
+                .then(function (result) {
+                    $scope.repFound = result.data.representativeFound;
+                    $scope.repInfo = result.data.representativeInfo;
+                    $scope.addrForm = false;
+                    if ($scope.repFound) {
+                        $scope.repForm = true;
+                    } else {
+                        $scope.repNotFoundForm = true;
+                    }
+                })
+                .catch(function (data) {
+                    console.log(data);
+                });
         };
 
-        $http.get('/locateLegislator', {params: coordinates})
-        .then(function(result) {
-            $scope.repFound = result.data.representativeFound;
-            $scope.repInfo = result.data.representativeInfo;
-
-            $scope.addrForm = false;
-            if ($scope.repFound) {
-                $scope.repForm = true;
-            } else {
-                $scope.repNotFoundForm = true;
-            }
-        })
-        .catch(function(data) {
-            console.log(data);
-        });
-    };
-
-    $scope.addrForm = true;
-    $scope.repForm = false;
-    $scope.repNotFoundForm = false;
-})
-.name;
+        $scope.addrForm = true;
+        $scope.repForm = false;
+        $scope.repNotFoundForm = false;
+    })
+    .name;
