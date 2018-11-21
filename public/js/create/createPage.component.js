b79d3b049ead87fea171105e3f31670476432b86
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-20 16:54:57 -0800

    add scope variables and methods to control disabled status of legislature level selection checkboxes in campaign creation form

--- public/js/create/createPage.component.js
+++ public/js/create/createPage.component.js
@@ -1,58 +1,84 @@
 import angular from 'angular';
 
 export default angular.module('helloGov')
-.component('createPage', {
-    template: require('./createPage.html'),
-    controller: function($http, constants, $location, $window) {
-        'ngInject';
-        let self = this;
-        let isNew = true;
-        // FIXME: this is super brittle to get the campaignId like this, but we're not using angular's
-        // routing so it's not possible to get it using angular yet
-        let urlSplit = $location.absUrl().split('/');
+    .component('createPage', {
+        template: require('./createPage.html'),
+        controller: function ($http, constants, $location, $window, $scope) {
+            'ngInject';
+            let self = this;
+            let isNew = true;
+            $scope.federalSenateSelected = false;
+            $scope.federalHouseSelected = false;
+            $scope.stateSenateSelected = false;
+            $scope.stateAssemblySelected = false;
+            // FIXME: this is super brittle to get the campaignId like this, but we're not using angular's
+            // routing so it's not possible to get it using angular yet
+            let urlSplit = $location.absUrl().split('/');
 
-        // if on an edit page, then grab the campaignId from URL
-        if (urlSplit.indexOf('edit') !== -1) {
-            var campaignId = urlSplit[urlSplit.length - 2];
-            isNew = false;
+            // if on an edit page, then grab the campaignId from URL
+            if (urlSplit.indexOf('edit') !== -1) {
+                var campaignId = urlSplit[urlSplit.length - 2];
+                isNew = false;
 
-            $http.get(`${constants.API_ROOT}/campaigns/${campaignId}`)
-                .then((resp) => {
-                    self.campaign = resp.data;
-                })
-                .catch(() => {
-                    $window.location.href = '/home';
-                });
-        }
-
-        this.saveCampaign = function(publishFlag) {
-            self.campaign.publish = publishFlag;
-            if (isNew) {
-                $http.post(`${constants.API_ROOT}/campaigns`, self.campaign)
-                    .then(function(resp) {
-                        if (publishFlag) {
-                            $window.location.href = `${resp.data.result.url}/success`;
-                        } else {
-                            $window.location.href = '/home';
-                        }
+                $http.get(`${constants.API_ROOT}/campaigns/${campaignId}`)
+                    .then((resp) => {
+                        self.campaign = resp.data;
                     })
-                    .catch(function() {
-                        self.error = 'There was an error creating your campaign. Please try again later.';
-                    });
-            } else {
-                $http.patch(`${constants.API_ROOT}/campaigns/${campaignId}`, self.campaign)
-                    .then(function(resp) {
-                        if (publishFlag) {
-                            $window.location.href = `${resp.data.result.url}/success`;
-                        } else {
-                            $window.location.href = '/home';
-                        }
-                    })
-                    .catch(function() {
-                        self.error = 'There was an error saving your campaign. Please try again later.';
+                    .catch(() => {
+                        $window.location.href = '/home';
                     });
             }
-        };
-    }
-})
-.name;
+
+            $scope.federalLegislatureSelected = function () {
+                return $scope.federalSenateSelected || $scope.federalHouseSelected;
+            };
+
+            $scope.stateLegislatureSelected = function () {
+                return $scope.stateSenateSelected || $scope.stateAssemblySelected;
+            };
+
+            $scope.updateLegislatureSelection = function () {
+                const checked = event.target.checked;
+                switch (event.target.name) {
+                case 'federal-senate':
+                    return $scope.federalSenateSelected = checked;
+                case 'federal-house':
+                    return $scope.federalHouseSelected = checked;
+                case 'state-senate':
+                    return $scope.stateSenateSelected = checked;
+                case 'state-assembly':
+                    return $scope.stateAssemblySelected = checked;
+                }
+            };
+
+            this.saveCampaign = function (publishFlag) {
+                self.campaign.publish = publishFlag;
+                if (isNew) {
+                    $http.post(`${constants.API_ROOT}/campaigns`, self.campaign)
+                        .then(function (resp) {
+                            if (publishFlag) {
+                                $window.location.href = `${resp.data.result.url}/success`;
+                            } else {
+                                $window.location.href = '/home';
+                            }
+                        })
+                        .catch(function () {
+                            self.error = 'There was an error creating your campaign. Please try again later.';
+                        });
+                } else {
+                    $http.patch(`${constants.API_ROOT}/campaigns/${campaignId}`, self.campaign)
+                        .then(function (resp) {
+                            if (publishFlag) {
+                                $window.location.href = `${resp.data.result.url}/success`;
+                            } else {
+                                $window.location.href = '/home';
+                            }
+                        })
+                        .catch(function () {
+                            self.error = 'There was an error saving your campaign. Please try again later.';
+                        });
+                }
+            };
+        }
+    })
+    .name;
