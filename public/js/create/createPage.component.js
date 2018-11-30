33063aa7ec271740ba7aef502ce18db0f551e256
tpetersen0308 <travis.e.petersen@gmail.com>
2018-11-29 18:44:52 -0800

    add scope variable stateNames to contain names of all US states

--- public/js/create/createPage.component.js
+++ public/js/create/createPage.component.js
@@ -11,6 +11,8 @@ export default angular.module('helloGov')
             $scope.federalHouseSelected = false;
             $scope.stateSenateSelected = false;
             $scope.stateAssemblySelected = false;
+            $scope.stateNames = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado', 'Connecticut', 'Delaware', 'District of Columbia', 'Federated States of Micronesia', 'Florida', 'Georgia', 'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota', 'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Island', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
             // FIXME: this is super brittle to get the campaignId like this, but we're not using angular's
             // routing so it's not possible to get it using angular yet
             let urlSplit = $location.absUrl().split('/');
